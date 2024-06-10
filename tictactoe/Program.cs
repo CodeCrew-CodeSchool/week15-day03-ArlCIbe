c55073cf73dd3699760bfbcac747683a2aa0d0bf
Arlinda C Ibezim <ArlCIbe@outlook.com>
2024-06-09 22:17:21 -0500

    added player, game, gameboard, and position classes

--- tictactoe/Program.cs
+++ tictactoe/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hi! Welcome to your game of Tic-Tac-Toe.\n Here's your starting board:\n");
+        Console.WriteLine
     }
+}
+
+class Player {
+// defines the fields and methods each object(aka instance) created from the Player class will have
+
+    public string player
+
+    public string marker;
+// properties each instance will have
+}
+
+class GameBoard {
+
+}
+
+class Game {
+
+} 
+
+class Position {
+
 }
