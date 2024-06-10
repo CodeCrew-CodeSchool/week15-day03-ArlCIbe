89804bd96979cfdd2ec15742b1908c3273fbe8d5
Arlinda C Ibezim <ArlCIbe@outlook.com>
2024-06-10 12:36:03 -0500

    edited Player class and its instances

--- tictactoe/Program.cs
+++ tictactoe/Program.cs
@@ -1,8 +1,18 @@
-﻿namespace tictactoe;
+﻿using System.Globalization;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace tictactoe;
+
+/* class - defines (aka declares) the properties (aka combo of INSTANCE FIELDS & INSTANCE METHODS) of its objects(aka INSTANCES)
+    - instance - a singular object assembled from the properties designated by its class; though each object that a class produces has the same properties, the value of those properties are different for each object
+        - instance field - variable that defines a single detail of an object
+        - instance method - function that defines a thing/things that an object can do
+*/
 
 class Program
 {
-    static void Main(string[] args)
+    static bool Main(string[] args)
     {
         Console.WriteLine("Hi! Welcome to your game of Tic-Tac-Toe.\n");
 
@@ -12,18 +22,51 @@ class Program
         Console.WriteLine("|4||5||6|");
         Console.WriteLine("|5||6||7|\n");
 
-        Console.WriteLine("Player 1, press any key to start the g.");
+        Console.WriteLine("The first player able to get three markers in a diagonal, vertical, or horizontal line wins.\n");
 
-    }
+        string name = Console.ReadLine();
+        string marker = Console.ReadLine();
+
+        Players[] player = new Players[3];
+        Players[] number = new Players[2];
+        Players[] markers = new Players[2];
+
+        player[0] = new Players($"{name}, {marker}, {number}");
+        player[1] = new Players($"{name}, {marker}, {number}"); 
+
+        markers[0] = new Players($"{player[0].Marker}");
+        markers[1] = new Players($"{player[1].Marker}");
+
+        number[0] = new Players($"{player[0].Name}");
+        number[1] = new Players($"{player[1].Name}"); 
+
+        Console.Write($"Player 1, please enter your name: {name}");
+
+        Console.Write($"{player[0].Name}, please enter either X or O to choose your marker: {marker}\n");
+
+        Console.WriteLine($"{player[0].Name}, your marker is {player[0].Marker}.\n");
+      
+        Console.WriteLine($"Player 2, please enter your name: {name}\n");
+
+        Console.WriteLine($"{player[1].Name}, your marker is {player[1].Marker}. Press any key to start the game.\n");
+        
 }
 
-class Player {
-// defines the fields and methods each object(aka instance) created from the Player class will have
+class Players {
 
-    public string player;
+    public string Name;
+    public string[] Markers;
+    public string Marker;
+    public int[] Number;
+    //instance fields of each Player instance
 
-    public string marker;
-// properties each instance will have
+    public Players(string name, string[] markers, int[] number, string marker){
+    // constructor method; function used to initialize Player's fields (aka declare them and assign them a value at the same time)
+        Name = name;
+        Markers = markers;
+        Number = number;
+        Marker = marker;
+    }
 }
 
 class GameBoard {
--- tictactoe/obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
+++ tictactoe/obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c55073cf73dd3699760bfbcac747683a2aa0d0bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a0f546aa24b66c48258bbf1db1840597af91e31")]
 [assembly: System.Reflection.AssemblyProductAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
