bafc48932627538f12cbd1cab0d2f5f31429040e
Arlinda C Ibezim <ArlCIbe@outlook.com>
2024-07-08 11:20:27 -0500

    class starter code & my first attempt without it

--- tictactoe/obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
+++ tictactoe/obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a0f546aa24b66c48258bbf1db1840597af91e31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89804bd96979cfdd2ec15742b1908c3273fbe8d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
